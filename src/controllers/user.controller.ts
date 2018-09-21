6576f504b7e560ecc19069adf5e4a0d47e5f66aa
SilentWolf <kezyolanipekun@gmail.com>
2018-09-21 06:10:07 +0100

    user CRUD done, change password, forget password, authenticate and generate token done

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,15 +3,15 @@ import {
     ParseIntPipe, Body, Put, Delete, Patch,
 } from '@nestjs/common';
 import {UserService} from '../services/user.service';
-import {ApiBearerAuth, ApiOAuth2Auth, ApiOperation, ApiUseTags} from '@nestjs/swagger';
+import {ApiOAuth2Auth, ApiOperation, ApiUseTags} from '@nestjs/swagger';
 import {messages} from '../config/messages.conf';
 import {RestfulRes} from '../response/restful.res';
 import {UserUpdateReq} from '../requests/user.update.req';
 import {UserReq} from '../requests/user.req';
 import {ServicesService} from '../services/services.service';
 import {ChangePasswordReq} from '../requests/change.password.req';
-import {UserEnum} from "../enums/user.enum";
-import {Roles} from "../shared/decorators/roles.decorator";
+import {UserEnum} from '../enums/user.enum';
+import {Roles} from '../shared/decorators/roles.decorator';
 
 @ApiUseTags('users')
 @Controller('users')
@@ -26,9 +26,11 @@ export class UserController {
         return data ? RestfulRes.success(res, messages.users.created, data) : RestfulRes.error(res, messages.operationFailed);
     }
 
+    @ApiOAuth2Auth()
+    @Roles(UserEnum.CLIENT_USER)
     @Put(':id')
     @ApiOperation({title: 'Phone number is optional'})
-    async update(@Response() res, @Request() req, @Param('id', new ParseIntPipe()) id: number, @Body() user_reg: UserUpdateReq) {
+    async update(@Response() res, @Request() req,  @Headers('Authorization') authorization: string, @Param('id', new ParseIntPipe()) id: number, @Body() user_reg: UserUpdateReq) {
         const data = await this.userService.update(user_reg);
         return data ? RestfulRes.success(res, messages.users.updated, data) : RestfulRes.error(res, messages.operationFailed);
     }
@@ -42,31 +44,39 @@ export class UserController {
     @ApiOAuth2Auth()
     @Roles(UserEnum.CLIENT_USER)
     @Patch(':id/change_password')
-    async changePassword(@Response() res,  @Headers('Authorization') authorization: string, @Request() req, @Param('id', new ParseIntPipe()) id: number, @Body() passwordSettings: ChangePasswordReq) {
+    async changePassword(@Response() res, @Headers('Authorization') authorization: string, @Request() req, @Param('id', new ParseIntPipe()) id: number, @Body() passwordSettings: ChangePasswordReq) {
         const data = await this.servicesService.changePassword(passwordSettings);
         return data ? RestfulRes.success(res, messages.passwordChanged, data) : RestfulRes.error(res, messages.passwordFailed);
     }
 
+    @ApiOAuth2Auth()
+    @Roles(UserEnum.CLIENT_USER)
     @Get(':id/check-in-history')
-    async findAllUserQueries(@Response() res, @Request() request, @Param('id', new ParseIntPipe()) id: number) {
+    async findAllUserQueries(@Response() res, @Headers('Authorization') authorization: string, @Request() request, @Param('id', new ParseIntPipe()) id: number) {
         const data = await this.userService.getUserCheckInHistory();
         return data ? RestfulRes.success(res, messages.history.list.success, data) : RestfulRes.error(res, messages.history.list.failed);
     }
 
+    @ApiOAuth2Auth()
+    @Roles(UserEnum.SYSADMIN)
     @Get('/check-in-history')
-    async findAllQueries(@Response() res, @Request() request) {
+    async findAllQueries(@Response() res, @Headers('Authorization') authorization: string, @Request() request) {
         const data = await this.userService.getCheckInHistory();
         return data ? RestfulRes.success(res, messages.history.list.success, data) : RestfulRes.error(res, messages.history.list.failed);
     }
 
+    @ApiOAuth2Auth()
+    @Roles(UserEnum.SYSADMIN)
     @Delete(':id')
-    async removeUser(@Response() res, @Request() req, @Param('id', new ParseIntPipe()) id: number) {
+    async removeUser(@Response() res, @Request() req, @Headers('Authorization') authorization: string, @Param('id', new ParseIntPipe()) id: number) {
         const data = await this.userService.remove(id);
         return data ? RestfulRes.success(res, messages.deleteSuccess, data) : RestfulRes.error(res, messages.operationFailed);
     }
 
+    @ApiOAuth2Auth()
+    @Roles(UserEnum.SYSADMIN)
     @Get(':id')
-    async fetchAUser(@Response() res, @Param('id', new ParseIntPipe()) id: number) {
+    async fetchAUser(@Response() res, @Headers('Authorization') authorization: string, @Param('id', new ParseIntPipe()) id: number) {
         const data = await this.userService.getUserById(id);
         return data ? RestfulRes.success(res, messages.users.one.success, data) : RestfulRes.error(res, messages.users.one.failed);
     }
