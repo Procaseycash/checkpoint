d37ab09744fecb6b9a947cc49ee32aab999cf623
SilentWolf <kezyolanipekun@gmail.com>
2018-09-21 06:16:32 +0100

    user seed updated

--- src/seeders/user-seed.ts
+++ src/seeders/user-seed.ts
@@ -1,10 +1,11 @@
 import * as faker from 'faker';
 import {password} from '../utils/utils';
 import {SEED_LENGTH} from '../config/app.config';
+import {UserEnum} from '../enums/user.enum';
 
 const USERS = [];
 for (let i = 0; i < 5; i++) {
-    const userType = ['QUERER', 'SYSADMIN'];
+    const userType = [UserEnum.CLIENT_USER, UserEnum.SYSADMIN];
     const rand = Math.floor(Math.random() * 2);
     const user = {
         _id: i + 1,
@@ -13,29 +14,18 @@ for (let i = 0; i < 5; i++) {
         last_name: faker.name.lastName(),
         phone_no: faker.phone.phoneNumber(),
         email: faker.internet.email().toLowerCase(),
-        type: 'SYSADMIN',
+        type: UserEnum.CLIENT_USER,
     };
     USERS.push(user);
 }
-USERS.unshift({
-    _id: SEED_LENGTH + 1,
-    password: password.hash('password'),
-    first_name: 'Ilo',
-    last_name: 'Calistus',
-    phone_no: '07039056183',
-    email: 'ilo@tunga.io',
-    type: 'SYSADMIN',
-    created_at: faker.date.recent(),
-    updated_at: faker.date.recent(),
-});
 USERS.unshift({
     _id: SEED_LENGTH + 2,
     password: password.hash('password'),
     first_name: 'Admin',
     last_name: 'Admin',
     phone_no: '09088998899',
-    email: 'admin@cba.in',
-    type: 'SYSADMIN',
+    email: 'admin@checkin.com',
+    type: UserEnum.SYSADMIN,
     created_at: faker.date.recent(),
     updated_at: faker.date.recent(),
 });
@@ -46,7 +36,7 @@ USERS.unshift({
     last_name: 'Olanipekun',
     phone_no: '08135061846',
     email: 'kezyolanipekun@gmail.com',
-    type: 'SYSADMIN',
+    type: UserEnum.SYSADMIN,
     created_at: faker.date.recent(),
     updated_at: faker.date.recent(),
 });
