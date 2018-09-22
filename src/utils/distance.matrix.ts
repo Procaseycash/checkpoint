762429455b9e1a0118f28ee3f387f70357140113
SilentWolf <kezyolanipekun@gmail.com>
2018-09-22 05:54:08 +0100

    update done and development in progress

--- src/utils/distance.matrix.ts
+++ src/utils/distance.matrix.ts
@@ -3,7 +3,7 @@ import * as distance from 'google-distance-matrix';
 export class DistanceMatrix {
     private static modes = ['driving', 'walking', 'bicycling', 'transit'];
 
-    public static async matrix(mode: string, ...origins: string[], ...destinations: string[]) {
+    public static async matrix(origin: string, destination: string) {
 
     }
 
